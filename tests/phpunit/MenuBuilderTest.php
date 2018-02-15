c4eb15654eaba070000ed634ee720d71e89c11b7
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2018-02-15 16:55:50 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 16.0.0
    Change-Id: Ibb3b0d2f4713609cd43d8c411561ab0bcdf941ce

--- tests/phpunit/MenuBuilderTest.php
+++ tests/phpunit/MenuBuilderTest.php
@@ -8,7 +8,7 @@ use MediaWiki\Minerva\MenuEntry;
 /**
  * @group MinervaNeue
  */
-class MenuTest extends \PHPUnit_Framework_TestCase {
+class MenuTest extends \PHPUnit\Framework\TestCase {
 	private $homeComponent = [
 		'text' => 'Home',
 		'href' => '/Main_page',
