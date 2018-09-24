58c0b8131f2eb7c0206a2761989899346fec6187
jdlrobson <jdlrobson@gmail.com>
2018-09-24 14:49:35 -0700

    Minerva A/B tests are not subject to HTML caching time
    Config added inside SkinMinerva is subject to the rules of HTML
    caching and can take several days [1]
    
    Adding it via the onResourceLoaderGetConfigVars hook reduces the
    cache time to minutes [2], giving us more control over the experiment's
    enabled state.
    [1] T124954
    [2] https://www.mediawiki.org/wiki/ResourceLoader
    
    Bug: T205355
    Change-Id: I18cb40afe405e2f6a89a7ce5a924eb855aed5430

--- includes/skins/SkinMinerva.php
+++ includes/skins/SkinMinerva.php
@@ -1268,7 +1268,6 @@ class SkinMinerva extends SkinTemplate implements ICustomizableSkin {
 			'wgMinervaDownloadNamespaces' => $this->getConfig()->get( 'MinervaDownloadNamespaces' ),
 			'wgMinervaMenuData' => $this->getMenuData(),
 			'wgMFDescription' => $out->getProperty( 'wgMFDescription' ),
-			'wgMinervaABSamplingRate' => $this->getConfig()->get( 'MinervaABSamplingRate' )
 		];
 
 		if ( $this->isAuthenticatedUser() ) {
