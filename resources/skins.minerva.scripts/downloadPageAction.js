c73ff1f7a5c258af9a05fe6b45112d392cba97f3
jdlrobson <jdlrobson@gmail.com>
2018-11-07 16:06:54 -0800

    Composition: DownloadIcon
    The DownloadIcon is reduced to a factory function that
    returns an instance of Icon
    
    Depends-On: I4d703eef68d51bbe0b03579c5cca0845e17b8c9d
    Depends-On: I4a4129b2cac7c7c49559beef0b8780f3211edf9c
    Bug: T205592
    Change-Id: Ib87390d17bef6f50842f52cd84c9ce2b162aaff0

---
+++ resources/skins.minerva.scripts/downloadPageAction.js
@@ -0,0 +1,153 @@
+( function ( M, track, msg ) {
+	var helpers,
+		MAX_PRINT_TIMEOUT = 3000,
+		GLYPH = 'download',
+		icons = M.require( 'mobile.startup/icons' ),
+		Icon = M.require( 'mobile.startup/Icon' ),
+		browser = M.require( 'mobile.startup/Browser' ).getSingleton();
+
+	/**
+	 * Helper function to retrieve the Android version
+	 * @ignore
+	 * @param {string} userAgent User Agent
+	 * @return {number|false} An integer.
+	 */
+	function getAndroidVersion( userAgent ) {
+		var match = userAgent.toLowerCase().match( /android\s(\d\.]*)/ );
+		return match ? parseInt( match[1] ) : false;
+	}
+
+	/**
+	 * Helper function to retrieve the Chrome/Chromium version
+	 * @ignore
+	 * @param {string} userAgent User Agent
+	 * @return {number|false} An integer.
+	 */
+	function getChromeVersion( userAgent ) {
+		var match = userAgent.toLowerCase().match( /chrom(e|ium)\/(\d+)\./ );
+		return match ? parseInt( match[2] ) : false;
+	}
+
+	helpers = {
+		/**
+		 * Checks whether DownloadIcon is available for given user agent
+		 *
+		 * @memberof DownloadIcon
+		 * @instance
+		 * @param {Window} windowObj
+		 * @param {Page} page to download
+		 * @param {string} userAgent User agent
+		 * @param {number[]} supportedNamespaces where printing is possible
+		 * @return {boolean}
+		*/
+		isAvailable: function ( windowObj, page, userAgent, supportedNamespaces ) {
+			var androidVersion = getAndroidVersion( userAgent ),
+				chromeVersion = getChromeVersion( userAgent );
+
+			// Download button is restricted to certain namespaces T181152.
+			// Defaults to 0, in case cached JS has been served.
+			if ( supportedNamespaces.indexOf( page.getNamespaceId() ) === -1 ||
+				page.isMainPage() ) {
+				// namespace is not supported or it's a main page
+				return false;
+			}
+
+			if ( browser.isIos() || chromeVersion === false ||
+				windowObj.chrome === undefined
+			) {
+				// we support only chrome/chromium on desktop/android
+				return false;
+			}
+			if ( ( androidVersion && androidVersion < 5 ) || chromeVersion < 41 ) {
+				return false;
+			}
+			return true;
+		},
+		/**
+		 * onClick handler for button that invokes print function
+		 * @param {Skin} skin
+		 * @param {Icon} icon
+		 * @param {Icon} spinner
+		 */
+		onClick: function ( skin, icon, spinner ) {
+			function doPrint() {
+				icon.timeout = clearTimeout( icon.timeout );
+				track( 'minerva.downloadAsPDF', {
+					action: 'callPrint'
+				} );
+				window.print();
+				icon.$el.show();
+				spinner.$el.hide();
+			}
+
+			function doPrintBeforeTimeout() {
+				if ( icon.timeout ) {
+					doPrint();
+				}
+			}
+			// The click handler may be invoked multiple times so if a pending print is occurring
+			// do nothing.
+			if ( !icon.timeout ) {
+				track( 'minerva.downloadAsPDF', {
+					action: 'fetchImages'
+				} );
+				icon.$el.hide();
+				spinner.$el.show();
+				// If all image downloads are taking longer to load then the MAX_PRINT_TIMEOUT
+				// abort the spinner and print regardless.
+				icon.timeout = setTimeout( doPrint, MAX_PRINT_TIMEOUT );
+				skin.loadImagesList().then( doPrintBeforeTimeout, doPrintBeforeTimeout );
+			}
+		}
+	};
+
+	/**
+	 * Gets a click handler for the download icon
+	 * Expects to be run in the context of an icon using `Function.bind`
+	 *
+	 * @param {Skin} skin
+	 * @param {Icon} spinner
+	 * @returns {function}
+	 */
+	function getOnClickHandler( skin, spinner ) {
+		return function () {
+			helpers.onClick( skin, this, spinner );
+		};
+	}
+
+	/**
+	 * Generate a download icon for triggering print functionality if
+	 * printing is available
+	 *
+	 * @param {Skin} skin
+	 * @param {number[]} supportedNamespaces
+	 * @param {Window} [windowObj] window object
+	 * @returns {jQuery.Object|null}
+	 */
+	function downloadPageAction( skin, supportedNamespaces, windowObj ) {
+		var icon, spinner = icons.spinner();
+		if (
+			helpers.isAvailable(
+				windowObj, skin.page, navigator.userAgent,
+				supportedNamespaces
+			)
+		) {
+			icon = new Icon( {
+				glyphPrefix: 'minerva',
+				title: msg( 'minerva-download' ),
+				name: GLYPH,
+				events: {
+					// will be bound to `this`
+					click: getOnClickHandler( skin, spinner )
+				}
+			} );
+			return $( '<li>' ).append( icon.$el ).append( spinner.$el.hide() );
+		} else {
+			return null;
+		}
+	}
+
+	M.define( 'skins.minerva.scripts/test/getOnClickHandler', getOnClickHandler );
+	M.define( 'skins.minerva.scripts/test/isAvailable', helpers.isAvailable );
+	M.define( 'skins.minerva.scripts/downloadPageAction', downloadPageAction );
+}( mw.mobileFrontend, mw.track, mw.msg ) );
