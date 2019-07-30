dfb3e0b8c75e3366a9cc3da2104df23743bf020e
jdlrobson <jdlrobson@gmail.com>
2019-07-30 15:40:55 -0700

    Restore MobileWebMainMenuClickTracking logging (old way)
    menu/schema is never imported so it never executed.
    Move it into initLogging which seems to the be the logic place
    for this code to execute
    
    Change-Id: Iec85548cf96fcc7ea96977d0aa89f601dbd8599f

--- resources/skins.minerva.scripts/initLogging.js
+++ resources/skins.minerva.scripts/initLogging.js
@@ -1,8 +1,14 @@
 // This initialises EventLogging for main menu and some prominent links in the UI.
 // This code should only be loaded on the Minerva skin, it does not apply to other skins.
 // @deprecated and to be removed the moment that T220016 is live.
-var mainMenu = require( './menu.js' );
+var mainMenu = require( './menu.js' ),
+	logging = require( './menu/schema.js' );
 
 module.exports = function () {
-	mainMenu.enableLogging();
+	mw.loader.using( [
+		'ext.eventLogging'
+	] ).then( function () {
+		logging();
+		mainMenu.enableLogging();
+	} );
 };
