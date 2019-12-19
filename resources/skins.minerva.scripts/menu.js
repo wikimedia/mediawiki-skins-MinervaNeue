3c5edfa5cdb8d21b90e8747f396649da7c5a53e8
jdlrobson <jdlrobson@gmail.com>
2019-12-19 12:10:09 -0800

    Simplify menu code
    Now the main menu is not using JavaScript we can simplify
    the menu code to apply only to the Echo navigation drawer.
    
    Bug: T225213
    Change-Id: If4831fc700c7df3a2a389b5f95b6fbaea4b7d954

--- resources/skins.minerva.scripts/menu.js
+++ resources/skins.minerva.scripts/menu.js
@@ -1,32 +1,9 @@
-var MainMenu = require( './menu/MainMenu.js' ),
-	mainMenu = createMainMenu();
-
-/**
- * N.B. that the activator - the UI element that the user must click in order to open the main
- * menu - is always `.header .main-menu-button`.
- *
- * @return {MainMenu}
- *
- * @ignore
- */
-function createMainMenu() {
-	return new MainMenu( '.header .main-menu-button' );
-}
+var BODY_NOTIFICATIONS_REVEAL_CLASS = 'navigation-enabled secondary-navigation-enabled';
 
 /**
  * Wire up the main menu
  */
 function init() {
-	/**
-	 * Close navigation if skin is tapped
-	 * @private
-	 */
-	function onSkinClick() {
-		mainMenu.closeNavigationDrawers();
-	}
-
-	// eslint-disable-next-line no-jquery/no-global-selector
-	$( '.mw-mf-page-center__mask' ).on( 'click', onSkinClick );
 
 	// See I09c27a084100b223662f84de6cbe01bebe1fe774
 	// will trigger every time the Echo notification is opened or closed.
@@ -34,14 +11,13 @@ function init() {
 	// on tablet in mobile mode.
 	mw.hook( 'echo.mobile' ).add( function ( isOpen ) {
 		if ( isOpen ) {
-			mainMenu.openNavigationDrawer( 'secondary' );
+			$( document.body ).addClass( BODY_NOTIFICATIONS_REVEAL_CLASS );
 		} else {
-			mainMenu.closeNavigationDrawers();
+			$( document.body ).removeClass( BODY_NOTIFICATIONS_REVEAL_CLASS );
 		}
 	} );
 }
 
 module.exports = {
-	mainMenu: mainMenu,
 	init: init
 };
