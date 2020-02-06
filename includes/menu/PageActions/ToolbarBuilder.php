39dec42793d4d1dc21ba3cf5f8210b5aae9c827e
Phuedx <samsmith@wikimedia.org>
2020-02-06 18:26:17 +0000

    Revert "Reduce 'userContributions' icon code"
    This reverts commit 7b4b65a30f0513e2d5f9458568a8dc6f3854bc49, which
    caused a regression in the user menu part of Advanced Mobile
    Contributions mode.
    
    Bug: T244436
    Change-Id: Ifce627dac35b2cd05e1c5ebe658534b6b8d0de88
    (cherry picked from commit 6e4c6c99bcc5d5b568d074d41a0cf63c796f0bbf)

--- includes/menu/PageActions/ToolbarBuilder.php
+++ includes/menu/PageActions/ToolbarBuilder.php
@@ -165,10 +165,7 @@ class ToolbarBuilder {
 			SpecialPage::getTitleFor( 'Contributions', $user->getName() )->getLocalURL() );
 		$entry->setTitle( $label )
 			->trackClicks( 'contributions' )
-			->setIcon( 'userContributions', 'element',
-				// FIXME: mw-ui-icon-minerva-contributions can be removed after cache has cleared
-				// (I6c908acd70c0dca5bcb1754d1b25d3da2389feb8)
-				'mw-ui-icon-with-label-desktop mw-ui-icon-minerva-contributions' );
+			->setIcon( 'contributions', 'element', 'mw-ui-icon-with-label-desktop' );
 		return $entry;
 	}
 
