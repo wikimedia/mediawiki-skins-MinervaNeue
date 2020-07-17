6b8f8568a1b34fb92877a7899d6f47acd30cda37
jdlrobson <jdlrobson@gmail.com>
2020-07-17 10:48:02 -0700

    Restrict the number of errors we log in a given session
    One IP address, logged 3,029 errors across 2 pages
    This is not helpful as it makes the error seem more widespread
    than it really is so let's not logged errors after the first 5.
    
    Change-Id: I8abfc3c3852827ab84a06740356e9b9f659a2d03

--- resources/skins.minerva.scripts/errorLogging.js
+++ resources/skins.minerva.scripts/errorLogging.js
@@ -1,6 +1,12 @@
 ( function ( track, config, trackSubscribe, user ) {
 	module.exports = function () {
 		var suffix = user.isAnon() ? '.anon' : '.loggedin',
+			// we will keep track of errors counted, so that we don't overcount problematic
+			// browsers/users which are generating multiple issues. See MAX_ERRORS.
+			reportedInThisSession = 0,
+			// Very conservative for now.
+			// After this many errors are tracked, no more will be logged.
+			MAX_ERRORS = 5,
 			COUNTER_NAME = 'counter.MediaWiki.minerva.WebClientError' + suffix;
 
 		/**
@@ -12,9 +18,15 @@
 		 */
 		function countError( topic, data ) {
 			var isLocalStorage = data && data.source === 'store-localstorage-update';
-			if ( !isLocalStorage ) {
+			// Some errors, for example the Leaflet "Set map center and zoom first"
+			// error (T257872) can trigger a high volume of client side error reports to the server.
+			// At time of writing 1 IP logged 245 errors in the same session and page and another
+			// logged 3,029 errors across 2 pages. To filter these kind of errors out, set a cap
+			// on the amount of errors that can be logged in a single session.
+			if ( !isLocalStorage && reportedInThisSession < MAX_ERRORS ) {
 				track( COUNTER_NAME, 1 );
 			}
+			reportedInThisSession++;
 		}
 
 		if ( config.get( 'wgMinervaCountErrors' ) ) {
