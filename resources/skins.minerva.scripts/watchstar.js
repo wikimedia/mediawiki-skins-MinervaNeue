92648cb9f20c6c81ae6f672e7f7e0b55a0b6a585
Dayllan Maza <dmaza@wikimedia.org>
2020-09-09 18:00:06 -0400

    watchstar: Add expiry support to watchpage.mw event
    The watchpage.mw event triggered on core now sends in the expiry value
    if the watchlist is being watched temporarily. This patch is to
    handle the star icon accordingly.
    
    Bug: T261970
    Depends-On: I9e31e41e3438ebc5e6e462f5136af5c36ba34194
    Change-Id: Ia47190c1aaf4492a6cc31873a5b3bec75f2fec86
    (cherry picked from commit f78d7c11e90d39aed46a079365310a9cba85f051)

--- resources/skins.minerva.scripts/watchstar.js
+++ resources/skins.minerva.scripts/watchstar.js
@@ -8,11 +8,21 @@ var WATCHED_CLASS = 'mw-ui-icon-wikimedia-unStar-progressive',
  * @param {jQuery.Object} $icon
  */
 module.exports = function init( $icon ) {
-	$icon.on( 'watchpage.mw', function ( _ev, action ) {
+	$icon.on( 'watchpage.mw', function ( _ev, action, expiry ) {
 		$( this ).find( 'a' ).removeClass(
 			[ WATCHED_CLASS, 'watched', TEMP_WATCHED_CLASS, UNWATCHED_CLASS ]
-		).addClass(
-			action === 'watch' ? [ WATCHED_CLASS, 'watched' ] : UNWATCHED_CLASS
-		);
+		).addClass( function () {
+			var classes = UNWATCHED_CLASS;
+			if ( action === 'watch' ) {
+				classes = [ 'watched' ];
+				if ( expiry !== null ) {
+					classes.push( TEMP_WATCHED_CLASS );
+				} else {
+					classes.push( WATCHED_CLASS );
+				}
+			}
+
+			return classes;
+		} );
 	} );
 };
