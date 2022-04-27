79d4130fccc913183f12a1b7c69c6f0533d82843
bwang <bwang@wikimedia.org>
2022-04-27 09:55:06 -0500

    Hard deprecate MobileMenu Hook
    Bug: T303309
    Change-Id: Ice45416c4af4e8fd4bcda158c5f9b81e6c8c9491

--- includes/Menu/PageActions/ToolbarBuilder.php
+++ includes/Menu/PageActions/ToolbarBuilder.php
@@ -21,7 +21,6 @@
 namespace MediaWiki\Minerva\Menu\PageActions;
 
 use ExtensionRegistry;
-use Hooks;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Minerva\LanguagesHelper;
 use MediaWiki\Minerva\Menu\Entries\IMenuEntry;
@@ -166,8 +165,6 @@ class ToolbarBuilder {
 			$user = $this->relevantUserPageHelper->getPageUser();
 			$group->insertEntry( $this->createContributionsPageAction( $user ) );
 		}
-
-		Hooks::run( 'MobileMenu', [ 'pageactions.toolbar', &$group ], '1.38' );
 
 		// We want the edit icon/action always to be the last element on the toolbar list
 		if ( $permissions->isAllowed( IMinervaPagePermissions::CONTENT_EDIT ) ) {
