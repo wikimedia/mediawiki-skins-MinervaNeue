cea344c1688da5aea95be436b2f51837577fac0d
thiemowmde <thiemo.kreuz@wikimedia.de>
2023-06-30 09:05:45 +0200

    MinervaUI: Remove unused code
    Same as Ie1c2e93 in MobileFrontend.
    
    Change-Id: I6508f5882609a0f528bcfa0eb66a5ddcb5e1b8b7

--- includes/MinervaUI.php
+++ includes/MinervaUI.php
@@ -54,27 +54,4 @@ class MinervaUI {
 		return $base . ' ' . $modifiers . ' ' . $additionalClassNames;
 	}
 
-	/**
-	 * Get CSS classes for a mediawiki ui semantic element
-	 * @param string $base The base class
-	 * @param string $modifier Type of anchor (progressive, constructive, destructive)
-	 * @param string $additionalClassNames additional class names you want to associate
-	 *  with the iconed element
-	 * @return string class name for use with HTML element
-	 */
-	public static function semanticClass( $base, $modifier, $additionalClassNames = '' ) {
-		$modifier = empty( $modifier ) ? '' : 'mw-ui-' . $modifier;
-		return $base . ' ' . $modifier . ' ' . $additionalClassNames;
-	}
-
-	/**
-	 * Get CSS classes for buttons
-	 * @param string $modifier Type of button (progressive, constructive, destructive)
-	 * @param string $additionalClassNames additional class names you want to associate
-	 *  with the button element
-	 * @return string class name for use with HTML element
-	 */
-	public static function buttonClass( $modifier = '', $additionalClassNames = '' ) {
-		return self::semanticClass( 'mw-ui-button', $modifier, $additionalClassNames );
-	}
 }
