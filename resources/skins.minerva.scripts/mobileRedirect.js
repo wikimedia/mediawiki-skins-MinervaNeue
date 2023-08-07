b0fa66d5376166a661dae5e763fde9361bae1665
Phuedx <phuedx@wikimedia.org>
2023-08-07 15:58:11 +0000

    Revert "Add metric platfrom event tracking on mobile desktop toggle"
    This reverts commit 7856f79d1161b5d41eb96eb3e754e04ee07d4e9b.
    
    Reason for revert: This version of the instrument isn't in use. It was
    enabled on the Beta Cluster but then superseded in I39adbce1f2c8.
    
    Change-Id: I273a6f21a881f69298d6219e435639ced0fe8afe

--- resources/skins.minerva.scripts/mobileRedirect.js
+++ resources/skins.minerva.scripts/mobileRedirect.js
@@ -84,10 +84,6 @@ module.exports = function ( amcOutreach, currentPage ) {
 			return;
 		}
 
-		// Dispatch desktop toggle event.
-		// https://phabricator.wikimedia.org/T310852
-		mw.eventLog.dispatch( 'mediawiki.desktop_link.click' );
-
 		return executeWrappedEvent();
 	}
 
