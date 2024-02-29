09500a940cd38e3f1531f28b611977d0c7b1b573
Jon Robson <jdlrobson@gmail.com>
2024-02-29 13:09:58 -0800

    Default to day mode
    Bug: T358811
    Change-Id: Iadf061b85e3cff4b2e1fc6bda6ff034b5f71b918

--- tests/phpunit/skins/SkinMinervaTest.php
+++ tests/phpunit/skins/SkinMinervaTest.php
@@ -189,7 +189,7 @@ class SkinMinervaTest extends MediaWikiIntegrationTestCase {
 		$skin = new SkinMinerva();
 
 		$classes = $skin->getHtmlElementAttributes()['class'];
-		$this->assertStringContainsString( 'skin-night-mode-clientpref-2', $classes );
+		$this->assertStringContainsString( 'skin-night-mode-clientpref-0', $classes );
 	}
 
 	/**
@@ -219,7 +219,7 @@ class SkinMinervaTest extends MediaWikiIntegrationTestCase {
 		$skin->setContext( $context );
 
 		$classes = $skin->getHtmlElementAttributes()['class'];
-		$this->assertStringContainsString( 'skin-night-mode-clientpref-2', $classes );
+		$this->assertStringContainsString( 'skin-night-mode-clientpref-0', $classes );
 	}
 
 	/**
