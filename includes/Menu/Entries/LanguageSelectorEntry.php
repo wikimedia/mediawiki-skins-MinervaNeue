b7e54644b7faab63962e115e2fe3a7fed7fbfdc9
Steph Toyofuku <stoyofuku@wikimedia.org>
2024-05-13 14:53:31 -0700

    Make language selector disabled state accessible contrast
    Remove the opacity filter and indicate a disabled state using the
    disabled color for both text and the icon.  Add a new disabled icon
    variant to accomplish this
    
    Also fixes a small issue where the toast notification was unreadable in
    night mode due to using --color-inverted.  Switch this to
    --color-inverted-fixed, which was added for this purpose
    
    Visual change for the language selector button, when there are no other
    languages available
    
    Bug: T361693
    Change-Id: Id2f80028d967bf644673f651bb89718fa8723d22

--- includes/Menu/Entries/LanguageSelectorEntry.php
+++ includes/Menu/Entries/LanguageSelectorEntry.php
@@ -56,7 +56,7 @@ class LanguageSelectorEntry implements IMenuEntry {
 		$this->title = $title;
 		$this->doesPageHaveLanguages = $doesPageHaveLanguages;
 		$this->messageLocalizer = $messageLocalizer;
-		$this->icon = 'language-base20';
+		$this->icon = $doesPageHaveLanguages ? 'language-base20' : 'language-disabled';
 		$this->label = $label;
 		$this->classes = $classes;
 	}
