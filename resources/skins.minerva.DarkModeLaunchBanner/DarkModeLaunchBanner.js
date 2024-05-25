a16f637fdc5b604ad77858f412bf7ca34403ee7f
Fomafix <fomafix@googlemail.com>
2024-05-25 10:45:53 +0000

    SECURITY: Prevent JavaScript injections by messages
    This change prevents the possibility to inject JavaScript by
    skin-minerva-night-mode-launch messages introduced by change
    I3cfa9517a387df636534a3940e09289e4d7a1e5b.
    
    Change-Id: Ib43bcff1413e6b70cedb7db4fa2c68148c4f13f5

--- resources/skins.minerva.DarkModeLaunchBanner/DarkModeLaunchBanner.js
+++ resources/skins.minerva.DarkModeLaunchBanner/DarkModeLaunchBanner.js
@@ -5,10 +5,10 @@ const makeTemplate = function ( description ) {
         <header class="cdx-dialog__header cdx-dialog__header--default">
             <div class="cdx-dialog__header__title-group">
                 <h2 id="cdx-dialog-label-0" class="cdx-dialog__header__title">
-                    ${ mw.message( 'skin-minerva-night-mode-launch-title' ).text() }
+                    ${ mw.message( 'skin-minerva-night-mode-launch-title' ).escaped() }
                 </h2>
             </div>
-            <button class="cdx-button cdx-button--action-default cdx-button--weight-quiet cdx-button--size-medium cdx-button--icon-only cdx-dialog__header__close-button" type="button" aria-label="${ mw.message( 'skin-minerva-night-mode-launch-close-label' ).text() }">
+            <button class="cdx-button cdx-button--action-default cdx-button--weight-quiet cdx-button--size-medium cdx-button--icon-only cdx-dialog__header__close-button" type="button" aria-label="${ mw.message( 'skin-minerva-night-mode-launch-close-label' ).escaped() }">
                 <span class="cdx-icon cdx-icon--medium">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><title>Close</title><g><path d="m4.34 2.93 12.73 12.73-1.41 1.41L2.93 4.35z"></path><path d="M17.07 4.34 4.34 17.07l-1.41-1.41L15.66 2.93z"></path></g></svg>
                 </span>
@@ -18,7 +18,7 @@ const makeTemplate = function ( description ) {
         <div class="cdx-dialog__body">
             <div class="skin-minerva-launch-image"></div>
             <h3>
-            ${ mw.message( 'skin-minerva-night-mode-launch-subtitle' ).text() }
+            ${ mw.message( 'skin-minerva-night-mode-launch-subtitle' ).escaped() }
             </h3>
             <p>
                 ${ description }
@@ -27,7 +27,7 @@ const makeTemplate = function ( description ) {
         <footer class="cdx-dialog__footer cdx-dialog__footer--default">
             <div class="cdx-dialog__footer__actions">
                 <button class="cdx-button cdx-button--action-progressive cdx-button--weight-primary cdx-button--size-medium cdx-button--framed cdx-dialog__footer__primary-action">
-                    ${ mw.message( 'skin-minerva-night-mode-launch-settings-label' ).text() }
+                    ${ mw.message( 'skin-minerva-night-mode-launch-settings-label' ).escaped() }
                 </button>
             </div>
         </footer>
@@ -59,8 +59,8 @@ function init() {
 			colorMode === 'night' ||
 			( colorMode === 'auto' && matchMediaDark )
 		) ?
-			mw.message( 'skin-minerva-night-mode-launch-description-night' ).text() :
-			mw.message( 'skin-minerva-night-mode-launch-description-day' ).text(),
+			mw.message( 'skin-minerva-night-mode-launch-description-night' ).escaped() :
+			mw.message( 'skin-minerva-night-mode-launch-description-day' ).escaped(),
 		templateElement = makeTemplate( description ),
 		dialogOverlay = templateElement.querySelector( '.cdx-dialog-backdrop' ),
 		dialogCloseButton = templateElement.querySelector( '.cdx-dialog__header__close-button' ),
